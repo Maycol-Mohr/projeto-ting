c2ba5deb8644c167a9e54e4cc0adf51ccc4257df
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-05-07 17:42:11 +0200

    tentando exercicio 2 com ajuda

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write('Formato inválido\n')
+
+    if path_file != 'statics/arquivo_teste.txt':
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+
+    else:
+        arquivo = open(path_file, mode='r')
+        lista_vazia = []
+        linhas = arquivo.read().split('\n')
+        for linha in linhas:
+            lista_vazia.append(linha)
+        return lista_vazia
