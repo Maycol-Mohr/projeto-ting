77c38c3780e7d1a7a40b16d48e41e4f04ab0d950
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-05-08 21:38:37 +0200

    requisito 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -25,3 +25,22 @@ def exists_word(word, instance: Queue):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    lista_vazia = []
+
+    for indice in range(len(instance)):
+        arquivo = instance.search(indice)
+        ocorrencias = []
+        for index, linha in enumerate(arquivo['linhas_do_arquivo']):
+            if word.lower() in linha.lower():
+                ocorrencias.append({
+                    'linha': index + 1,
+                    'conteudo': linha
+                })
+        if ocorrencias:
+            lista_vazia.append({
+                "palavra": word,
+                "arquivo": arquivo['nome_do_arquivo'],
+                "ocorrencias": ocorrencias
+            })
+
+    return lista_vazia
