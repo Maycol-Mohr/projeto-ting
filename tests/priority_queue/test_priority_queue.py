8dec879db21eec620e6f0405d579a7131d75e790
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-05-09 01:06:19 +0200

    tentando exercicio 6 com ajuda

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,36 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+regular_priority = {
+        "nome_do_arquivo": "arquivo_teste1.txt",
+        "qtd_linhas": 33,
+        "linhas_do_arquivo": [...]
+    }
+
+high_priority = {
+        "nome_do_arquivo": "arquivo_teste3.txt",
+        "qtd_linhas": 3,
+        "linhas_do_arquivo": [...]
+    }
 
 
 def test_basic_priority_queueing():
     """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+
+    priority_queue.enqueue(regular_priority)
+
+    priority_queue.enqueue(high_priority)
+
+    assert len(priority_queue) == 2
+
+    assert priority_queue.search(0) == high_priority
+
+    assert priority_queue.dequeue() == high_priority
+
+    assert priority_queue.search(0) == regular_priority
+
+    assert len(priority_queue) == 1
+
+    with pytest.raises(IndexError):
+        priority_queue.search(99)
